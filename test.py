5df3c243fd62b03eea62d28eb5ee6733169c62fb
ArifAbdullaew <90764393+ArifAbdullaew@users.noreply.github.com>
2021-12-06 01:27:22 +0300

    Update test.py

--- test.py
+++ test.py
@@ -2,10 +2,9 @@ import telebot
 from forex_python.converter import CurrencyRates
 import requests as r
 
-bot = telebot.TeleBot(
-    '5097652539:AAFzQpvariYOIC8u70rSCzB1uRZ3CkpllYk', parse_mode='html')
+bot = telebot.TeleBot('5097652539:AAFzQpvariYOIC8u70rSCzB1uRZ3CkpllYk', parse_mode='html')
 
-COMMANDS = ['start', 'infos', 'infot', 'subject', 'date']
+COMMANDS = ['start', 'infos', 'infot', 'subject', 'date', 'starosta']
 
 
 @bot.message_handler(commands=COMMANDS)
@@ -17,19 +16,22 @@ def start_message(message) -> None:
     keyboard = get_main_keyboard()
 
     if message.text.lower() == '/start':
-        bot.send_message(message.chat.id, f'<b>{message.from_user.username}</b>,  \n\nДанный бот имеет множество различных полезных команд, все они приведены в списке ниже! \n\n/infos - информация о студентах!\n/infot - информация о преподавателях\n/date - даты ближайших работ!\n/subject - набор дисциплин 21/22!', reply_markup=keyboard)
+        bot.send_message(message.chat.id, f'<b>{message.from_user.first_name}</b>,  \n\nДанный бот имеет множество различных полезных команд, все они приведены в списке ниже! \n\n/infos - информация о студентах!\n/infot - информация о преподавателях\n/date - даты ближайших работ!\n/subject - набор дисциплин 21/22!\n/starosta - Кто является старостой и куратором БИБ211', reply_markup=keyboard)
 
     elif message.text.lower() == '/infos':
         infos(message)
 
     elif message.text.lower() == '/subject':
-        sub(message)
+        subject(message)
 
     elif message.text.lower() == '/infot':
         infot(message)
 
     elif message.text.lower() == '/date':
         date(message)
+        
+    elif message.text.lower() == '/starosta':
+        starosta(message)
 
 
 @bot.message_handler(content_types=['text'])
@@ -49,14 +51,17 @@ def text_processing(message) -> None:
 
     elif 'начало' in message.text.lower():
         keyboard = get_main_keyboard()
-        bot.send_message(message.chat.id, 'И снова здравствуй, и снова привет  \n\n/infos - информация о студентах!\n/infot - информация о преподавателях\n/date - даты ближайших работ!\n/subject - набор дисциплин 21/22!', reply_markup=keyboard)
+        bot.send_message(message.chat.id, 'И снова здравствуй, и снова привет  \n\n/infos - информация о студентах!\n/infot - информация о преподавателях\n/date - даты ближайших работ!\n/subject - набор дисциплин 21/22!\n/starosta - представительские лица', reply_markup=keyboard)
 
     elif 'предмет' in message.text.lower() or 'пред' in message.text.lower() or 'дисциплины' in message.text.lower():
-        sub(message)
+        subject(message)
 
+    elif 'зам' in message.text.lower() or 'стар' in message.text.lower() or 'староста' in message.text.lower() or 'куратор' in message.text.lower() or 'кураторы' in message.text.lower() or 'курат' in message.text.lower():
+        starosta(message)
+        
     else:
         bot.send_message(
-            message.chat.id, " Ну тут мои полномочия все \n тут уж извините, но все ")
+            message.chat.id, "\nНу тут мои полномочия все \n тут уж извините, но все ")
 
 
 def get_main_keyboard() -> telebot.types.ReplyKeyboardMarkup:
@@ -72,17 +77,29 @@ def get_main_keyboard() -> telebot.types.ReplyKeyboardMarkup:
 
 
 def date(message) -> None:
-
-    return ''
-
-
-def check_math_answer(message, answer) -> None:
-    return ''
+    try:
+        bot.send_message(message.chat.id, f'\n\nАлгоритмизация и программирование - 20.12.21\n\nИстория - 21.12.21\n\nИнформатика - 22.12.21\n\nМатематический анализ - 23.12.21')
+    except:
+        bot.send_message(message.chat.id, 'Я конечно люблю программировать, но не настолько, чтобы решать данную проблему👨‍💻', reply_markup=get_main_keyboard())
 
 
-def infos(message):
 
-    return  
+def infos(message) -> None:
+    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.row('Возвращаемся обратно 🚀 ')
+    bot.send_message(message.chat.id, f'\nАбдуллаев Ариф\n\nАбзах Баян Амер Мохд Амин\n\nАбраменко Александр\n\nАрхипочкин Александр\n\nБaлясникова Екатерина\n\nБелоус Александр\n\nВоронов Арсений\n\nГурбатова Елизавета\n\nГусев Максим\n\nДронова Алла\n\nЕлецкая Елизавета\n\nКайнова Мария\n\nКлепацкий Даниил\n\nКинякин Владислав\n\nКогут Михаил\n', reply_markup=keyboard)
+    bot.send_message(message.chat.id, f'\nКолдышев Пётр\n\nКрохин Алексей\n\nКрутоног Максим\n\nМиллер Ян\n\nПегай Владислав\n\nПлешков Иван\n\nСавин Даниил\n\nСемененя Александра\n\nТаньчев Алексей\n\nТимошкин Павел\n\nТуманов Александр\n\nФатхутдинов Никита зам. старосты \n\nХрупов Андрей\n\nЯрославский Александр староста \n\nМамута Александр\n', reply_markup=keyboard)
+        
+
+def starosta(message) -> None:
+        try:
+            keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+            keyboard.row('Возвращаемся обратно 🚀 ')
+            bot.send_message(message.chat.id, f'\nЯрославский Андрей - староста группы БИБ211\nПочта: avyaroslavskiy@edu.hse.ru\n\nФатхутдинов Никита - зам. старосты\nПочта: nifatkhutdinov@edu.hse.ru\n\nТамара Цкиманаури и Альберт Назаретян - кураторы группы БИБ211', reply_markup=keyboard)
+        except:
+            bot.send_message(message.chat.id, 'Братишка, ты что-то не то делаешь👮🏼‍♂️', reply_markup=get_main_keyboard())
+      
+      
 
 
 def infot(message) -> None:
@@ -102,9 +119,10 @@ def infot(message) -> None:
 def teacherinf(message) -> None:
     return ''
 
-
-def sub(message, pair) -> None:
-    return ''
+def subject(message) -> None:
+    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    bot.send_message(message.chat.id, f'\nМатематический Анализ\n\nИнформатика\n\nАлгоритмизация и Программирование\n\nПроектный семинар по Информационной безопасности\n\nКомпьютерный практикум Администрирование систем и сетей\n\nФизика\n\nИстория', reply_markup=keyboard)
+    
 
 
 bot.polling()
